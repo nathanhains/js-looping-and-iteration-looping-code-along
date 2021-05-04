27e4abba64796c1f4c7317ad64bd982035db122c
nathan hains <nathanhains14@yahoo.com>
2021-05-04 15:43:19 -0400

    commit tree 880349f7dd0e77acec406915fdfa5fef135b873b

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(array, s){
+    let new_array = [];
+    for(let i = 0; i<array.length; i++){
+        new_array.push("Thank you, "+`${array[i]}`+", for the wonderful "+`${s}`+" gift!")
+    }
+    return new_array;
+}
+
+function countDown(intega){
+    let i=intega;
+    while (i >=0){
+    console.log(i)
+    i--;
+    }
+}
